27dc84c25afdba32d4c54890bbadf05800d14aab
Jo Malo <juanitamalo.p@gmail.com>
2018-07-03 16:45:51 +0100

    saving bird sightings

--- server/server.js
+++ server/server.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const parser = require('body-parser');
 const MongoClient = require('mongodb').MongoClient;
 const createRouter = require('./helpers/create_router.js');
-const parser = require('body-parser');
 
 const publicPath = path.join(__dirname, '../client/public');
 app.use(express.static(publicPath));
+
+app.use(parser.json());
 
 MongoClient.connect('mongodb://localhost:27017', (err, client) => {
   if (err) {
@@ -18,8 +20,6 @@ MongoClient.connect('mongodb://localhost:27017', (err, client) => {
   const sightingsRouter = createRouter(sightingsCollection)
   app.use('/api/sightings', sightingsRouter);
 });
-
-app.use(parser.json());
 
 app.listen(3000, function () {
   console.log(`Listening on port ${ this.address().port }`);
